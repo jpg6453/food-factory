21ee05290a94271de054c27e11d880fb541dfb7d
Jon Gill <jonpgill1@gmail.com>
2021-10-02 11:16:06 +0000

    amend get_recipes functions to display last added recipe 1st

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def get_recipes():
         title = 'all'
         sub_title = main_ingredient
     else:
-        recipes = mongo.db.recipes.find()
+        recipes = mongo.db.recipes.find().sort('_id', -1)
         title = 'all'
         sub_title = ''
     return render_template('recipes.html', recipes=recipes, title=title, sub_title=sub_title)
