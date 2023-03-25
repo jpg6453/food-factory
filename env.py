03a7b7fa039b3642c2c8a084ca3a302865a07694
Jon Gill <jonpgill1@gmail.com>
2023-03-25 17:39:53 +0000

    update requirements txt

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['MONGO_URI'] = 'mongodb+srv://jpg6453:nikeSw00sh@myfirstcluster-bkdlo.mongodb.net/food_factory?retryWrites=true'
+                            
+os.environ['SECRET_KEY'] = '5891628ab0b13ce0c676dfde280ba145'
+                            
